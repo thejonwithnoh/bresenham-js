d1963dab34bf3005ebcddac2cd6c09c3f2bab6ed
Jonathan Faulch <jon@faulch.com>
2016-05-31 02:34:30 -0400

    Simplified various Array.map usages.

--- bresenham-js.js
+++ bresenham-js.js
@@ -3,17 +3,17 @@
 	root[propertyName] = function(pos1, pos2)
 	{
 		var delta = pos2.map(function(value, index) { return value - pos1[index]; });
-		var increment = delta.map(function(value) { return Math.sign(value); });
-		var absDelta = delta.map(function(value) { return Math.abs(value); });
+		var increment = delta.map(Math.sign);
+		var absDelta = delta.map(Math.abs);
 		var absDelta2 = absDelta.map(function(value) { return 2 * value; });
 		var maxIndex = absDelta.reduce(function(accumulator, value, index) { return value > absDelta[accumulator] ? index : accumulator; }, 0);
 		var error = absDelta2.map(function(value) { return value - absDelta[maxIndex]; });
 
 		var result = [];
-		var current = pos1.map(function(value) { return value; });
+		var current = pos1.slice();
 		for (var i = 0; i < absDelta[maxIndex]; i++)
 		{
-			result.push(current.map(function(value) { return value; }));
+			result.push(current.slice());
 			error.forEach(function(errorValue, index)
 			{
 				if (error[index] > 0)
@@ -24,7 +24,7 @@
 				error[index] += absDelta2[index];
 			});
 		}
-		result.push(current.map(function(value) { return value; }));
+		result.push(current.slice());
 		return result;
 	};
 }).apply(this, typeof module === 'undefined' ? [this, 'bresenham'] : [module, 'exports']);
